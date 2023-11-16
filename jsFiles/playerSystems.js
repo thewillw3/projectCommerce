69ae8236e98eb0535a9a70c30020879107557fb2
TheWillW <ytthewillw@gmail.com>
2023-11-16 14:02:39 -0500

    Continued work on playerSystems.js
    Added getter / setter for player name, getter for money, addMoney function, removeMoney function, modified addItem and removeItem, added getItem.

--- jsFiles/playerSystems.js
+++ jsFiles/playerSystems.js
@@ -6,9 +6,43 @@ $(function() {
         inventory: new Map(),
 
         /**
+         * Setter and getter for the player's name.
+         */
+        set uName(newName) {
+            this.name = newName;  
+        },
+
+        get uName() {
+            return this.name;
+        },
+
+        /**
+         * Getter for the player's money.
+         */
+        get uMoney() {
+            return this.money;
+        },
+
+        /**
+         * Adds [amount] of money to player's money count.
+         * @param {number} amount   Amount of money to add, must be >= 0. 
+         */
+        addMoney: function(amount) {
+            this.money += amount;
+        },
+
+        /**
+         * Removes [amount] of money from the player's money count.
+         * @param {number} amount   Amount of money to remove, must be >= 0.
+         */
+        removeMoney: function(amount) {
+            this.money = this.money - amount;
+        },
+
+        /**
          * Adds [amount] of item [id] to the player's inventory. 
-         * @param {number}  id      The name of an item in the game. 
-         * @param {string}  amount  The amount of item to add, must be >= 0. 
+         * @param {string}  id      The name of an item in the game. 
+         * @param {number}  amount  The amount of item to add, must be >= 0. 
          */
         addItem: function(id, amount) {
             if (this.inventory.has(id)) {
@@ -20,15 +54,24 @@ $(function() {
 
         /**
          * Removes [amount] of item [id] to the player's inventory. 
-         * @param {number}  id      The name of an item in the game. 
-         * @param {string}  amount  The amount of item to remove, must be >= 0. 
+         * @param {string}  id      The name of an item in the game. 
+         * @param {number}  amount  The amount of item to remove, must be >= 0. 
          */
         removeItem: function(id, amount) {
             if (this.inventory.has(id)) {
-                amount -= this.inventory.get(id);
+                amount = this.inventory.get(id) - amount;
             }
 
             this.inventory.set(id, amount);
+        },
+
+        /**
+         * Quickly obtains the number of item [id] the player has.
+         * @param {string}  id  The name of an item in the game.
+         * @returns {number}    The number of [id] in the player's inventory.
+         */
+        getItem: function(id) {
+            return this.inventory.get(id);
         }
     };
 });
